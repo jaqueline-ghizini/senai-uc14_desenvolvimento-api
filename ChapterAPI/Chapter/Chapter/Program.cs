951830d8aa94e2f7c176e5fe4978b5d74acc5068
jaqueline-ghizini <jaqueline.ghizini@outlook.com>
2022-12-03 01:54:43 -0300

    encontro remoto 4 - login

--- ChapterAPI/Chapter/Chapter/Program.cs
+++ ChapterAPI/Chapter/Chapter/Program.cs
@@ -1,6 +1,7 @@
 using Chapter.Contexts;
 using Chapter.Interfaces;
 using Chapter.Repositories;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,26 @@ builder.Services.AddCors(options =>
         .AllowAnyMethod();
     });
 });
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultChallengeScheme = "JwtBearer";
+    options.DefaultAuthenticateScheme = "JwtBearer";
+
+}).AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("chapter-chave-autenticacao")),
+        ClockSkew = TimeSpan.FromMinutes(60),
+        ValidIssuer = "chapter.webapi",
+        ValidAudience = "chapter.webapi"
+    };
+});
+
 
 builder.Services.AddScoped<ChapterContext, ChapterContext>();//adicionada em aula
 
@@ -40,6 +61,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
 
 app.MapControllers();
 
